ead3fcc0b30dc2c85ecd4c4c524a9a2d627fb7c1
Abdelkarim Khayi <159136936+Bakr1m@users.noreply.github.com>
2024-11-27 18:14:12 +0800

    Add files via upload

---
+++ qr_code_generator.py
@@ -0,0 +1,13 @@
+import qrcode
+from PIL import Image
+
+data = input('Enter the URL: ').strip()
+filename = input('Enter the filename: ').strip()
+
+qr = qrcode.QRCode(box_size = 10, border = 4)
+qr.add_data(data)
+
+image = qr.make_image(fill_color = 'black', back_color = 'white')
+image.save(filename)
+
+print(f'QR code saved as {filename}')
